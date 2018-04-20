1a5e703e302d92cd5b67a0e024236a95706b758d
Nicklas Kenyon <jusisgood45@gmail.com>
2018-04-20 12:14:18 -0400

    Perform text gen in edit mode

---
+++ Assets/Scripts/TextGen3D.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TextGen3D : MonoBehaviour {
+    public string text;
+    TextGenLex lex;
+    public float spacing;
+
+	// Use this for initialization
+	void Start () {
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ClearText();
+        GenerateText();
+    }
+
+    void ClearText()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            DestroyImmediate(transform.GetChild(i).gameObject);
+        }
+    }
+
+    void GenerateText()
+    {
+        // Grab the lex
+        lex = FindObjectOfType<TextGenLex>();
+
+        GameObject temp; // the game object for the letter
+        GameObject parent = new GameObject(text); // Parent for the text
+        parent.transform.parent = this.transform;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            // Get the prefab for the letter
+            temp = lex.GetCharacter(text[i]);
+
+            if (temp != null)
+            {
+                // Setup letter position
+                Vector3 pos = transform.position;
+                pos.x += spacing * i;
+
+                Instantiate(temp, pos, this.transform.rotation, parent.transform);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TextGenLex.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextGenLex : MonoBehaviour {
+	public TextGenLexEntry[] lex;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject GetCharacter(char c)
+    {
+        GameObject ret = null; // returned game object
+
+        for(int i = 0; i < lex.Length; i++)
+        {
+            if(lex[i].character == c)
+            {
+                ret = lex[i].prefab;
+                break;
+            }
+        }
+        return ret;
+    }
+}
+
+[System.Serializable]
+public struct TextGenLexEntry
+{
+    public GameObject prefab;
+    public char character;
+}
